2281e0542f56465b34429230bb59034d35c675b5
MiguelHuere <miguel.huere@unmsm.edu.pe>
2024-05-22 15:10:39 -0500

    Actulizacion

--- app.py
+++ app.py
@@ -8,16 +8,26 @@ from services.respuesta import respuestas  # Importar el blueprint de Respuestas
 from services.test_amasc import test_amasc_bp
 from services.test_beck import test_beck_bp
 from services.test_zung import test_zung_bp
-from config import DATABASE_CONNECTION
 from sqlalchemy import text
+from dotenv import load_dotenv
+import os
+import logging
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO)
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
 
 # Inicializar SQLAlchemy
+logging.info("Initializing database connection")
 db.init_app(app)
 
 # Registrar Blueprints
+logging.info("Registering blueprints")
 app.register_blueprint(estudiantes)
 app.register_blueprint(tests)
 app.register_blueprint(areas)
@@ -28,18 +38,22 @@ app.register_blueprint(test_beck_bp)
 app.register_blueprint(test_zung_bp)
 
 with app.app_context():
+    logging.info("Creating all database tables")
     db.create_all()
 
 #################### PARA PROBAR SI HAY CONEXIÓN
 @app.route('/check_db')
 def check_db():
     try:
+        logging.info("Checking database connection")
         with db.engine.connect() as connection:
             result = connection.execute(text('SELECT 1'))
             return 'Database connection successful!', 200
     except Exception as e:
+        logging.error(f"Database connection failed: {e}")
         return str(e), 500
 ##########################################
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    logging.info("Starting Flask application")
+    app.run(host='0.0.0.0', port=5000)
--- dockerfile
+++ dockerfile
@@ -6,4 +6,5 @@ RUN pip install -r requirements.txt
 COPY .env .env
 EXPOSE 5000
 
-CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0", "--port=5000"]
+
